86d516ccf6a65b103c44106b38c265ed229b84d5
Sergej Lopatkin <lopatkin.sergej@googlemail.com>
2026-02-11 14:08:50 +0100

    chore: cleanup

--- public/shaders/_chunk/blend/glow.glsl
+++
@@ -1,9 +0,0 @@
-// #include "./reflect.glsl"
-
-vec3 blendGlow(vec3 base, vec3 blend) {
-	return blendReflect(blend,base);
-}
-
-vec3 blendGlow(vec3 base, vec3 blend, float opacity) {
-	return (blendGlow(base, blend) * opacity + base * (1.0 - opacity));
-}
--- public/shaders/_chunk/blend/hard-light.glsl
+++
@@ -1,9 +0,0 @@
-// #include "./overlay.glsl"
-
-vec3 blendHardLight(vec3 base, vec3 blend) {
-	return blendOverlay(blend,base);
-}
-
-vec3 blendHardLight(vec3 base, vec3 blend, float opacity) {
-	return (blendHardLight(base, blend) * opacity + base * (1.0 - opacity));
-}
